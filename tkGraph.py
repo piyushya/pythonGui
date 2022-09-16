824a93fd91544d3e57d4040b2eee8618dc2d6fa0
Piyush Yadav <piyushya012@gmail.com>
2022-09-16 20:23:27 +0530

    added database

--- tkGraph.py
+++ tkGraph.py
@@ -1,70 +1,116 @@
 import tkinter as tk
+import sqlite3 as database
 from tkinter import Widget, ttk
 from ctypes import windll
 from tkinter.messagebox import showinfo
+from werkzeug.security import generate_password_hash, check_password_hash
 windll.shcore.SetProcessDpiAwareness(1)
 
-#learning
+class signUp(tk.Tk) :
+    def __init__(self) :
+        super().__init__()
+        self.title('User Interface 1.0')
+        windowWidth = 600
+        windowHeight = 400
+        padLeft = int(self.winfo_screenwidth()/2 - windowWidth/2)
+        padTop = int(self.winfo_screenheight()/2 - windowHeight/2)
 
-def launch(event) :
-    print("rocket lanuched")
+        self.geometry(f'{windowWidth}x{windowHeight}+{padLeft}+{padTop}')
+        self.resizable(False, False)
+        self.attributes('-alpha', 0.9)
+        self.iconbitmap('assets/icon.ico')
 
-def log(event):
-    print("logged")
+        self.label = ttk.Label(
+            self, 
+            text="SignUp Window",
+            background="white",
+            padding=10, 
+            width=100,
+            font=("Verdana", 14),
+            foreground="black"
+        )
+        self.label.pack()
 
-def download(event):
-    showinfo(
-        title='Information',
-        message='Download button clicked!'
-    )
-    with open("download.txt", "r+") as file :
-        content = file.read()
-        if not content :
-            file.write("1")
-            return
-        file.write(str(int(content)+1))
+        # store email address and password
+        self.email = tk.StringVar()
+        self.username = tk.StringVar()
+        self.password = tk.StringVar()
 
-root = tk.Tk()
-root.title('Rocket Interface 1.0')
+        email_fr = ttk.Frame(self)
+        username_fr = ttk.Frame(self)
+        password_fr = ttk.Frame(self)
 
-windowWidth = 600
-windowHeight = 400
-padLeft = int(root.winfo_screenwidth()/2 - windowWidth/2)
-padTop = int(root.winfo_screenheight()/2 - windowHeight/2)
+        email_label = ttk.Label(
+            email_fr, 
+            text="Email :", 
+            font=("Georgia", 12)
+        )
+        user_label = ttk.Label(
+            username_fr,
+            text="Username :",
+            font=("Georgia", 12)
+        )
+        password_label = ttk.Label(
+            password_fr,
+            text="Password :",
+            font=("Georgia", 12)
+        )
 
-root.geometry(f'{windowWidth}x{windowHeight}+{padLeft}+{padTop}')
-root.resizable(False, False)
-root.attributes('-alpha', 0.9)
-root.iconbitmap('assets/icon.ico')
+        email_entry = ttk.Entry(
+            email_fr, 
+            textvariable=self.email
+        )
+        user_entry = ttk.Entry(
+            username_fr, 
+            textvariable=self.username
+        )
+        password_entry = ttk.Entry(
+            password_fr, 
+            textvariable=self.password, 
+            show="*"
+        )
 
-download_icon = tk.PhotoImage(file='./assets/download.png')
-label = ttk.Label(
-    root, 
-    text="Rocket Dashboard", 
-    background="white", 
-    padding=10, 
-    width=100,
-    font=("Verdana", 14), foreground="black"
-)
-launch_button = ttk.Button(
-    root, 
-    text = 'Launch Rocket',
-    padding=10
-)
-download_button = ttk.Button(
-    root,
-    image=download_icon,
-    padding=5,
-    text='Download',
-    compound=tk.LEFT,
-)
+        email_fr.pack(expand=True)
+        username_fr.pack(expand=True)
+        password_fr.pack(expand=True)
 
-label.pack()
-launch_button.pack(expand=True)
-download_button.pack(expand=True)
+        
+        email_label.pack(side=tk.LEFT, expand=True)
+        user_label.pack(expand=True, side=tk.LEFT)
+        password_label.pack(expand=True, side=tk.LEFT)
+        
+        email_entry.pack(expand=True, padx=20)
+        user_entry.pack(expand=True, padx=20)
+        password_entry.pack(expand=True, padx=20)
 
-launch_button.bind('<Button>', launch)
-launch_button.bind('<Button>', log, add='+')
-download_button.bind('<Button>', download)
+        email_entry.focus()
+        # login button
+        login_button = ttk.Button(self, text="Signup", command=self.signup_clicked, padding=5)
+        login_button.pack(pady=30)
+    
+    def signup_clicked(self):
+            #callback when the login button clicked
+            if (not self.username.get()) or (not self.email.get()) or (not self.password.get()) :
+                showinfo(
+                    title='error',
+                    message="Empty field"
+                )
+                return
+            self.dbOps()
+            msg = f"Login Successfull for user : {self.username.get()}"
+            showinfo(
+                title='Information',
+                message=msg
+            )
+    def dbOps(self):
+        # database opeartion start
+        try :
+            conn = database.connect("data.db")
+            curr = conn.cursor()
+        except :
+            print("Error in database")
+        conn.close()
+        #database operation end
 
-root.mainloop()
+window = signUp()
+window.mainloop()
